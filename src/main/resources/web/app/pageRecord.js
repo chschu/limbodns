e626c6454a9dcc7ea19781a9d44f26a9c546c0bd
Christian Schuster <christian@dnup.de>
2024-01-29 17:07:39 +0100

    WIP: show hint for DynDNS2 usage

--- src/main/resources/web/app/pageRecord.js
+++ src/main/resources/web/app/pageRecord.js
@@ -34,7 +34,9 @@ app.component('pageRecord', {
 					ctrl.uurl.manu = ctrl.uurl.auto + "/2001:0db8:85a3:0000:0000:8a2e:0370:7334";
 					ctrl.uurl.wget = 'wget --inet6-only -qO - ' + ctrl.uurl.auto;
 				}
-				
+
+			  ctrl.uurl.dyndns2 = window.location.protocol + "//" + window.location.host + "/nic/update";
+
 		  });
 	  }
   }
